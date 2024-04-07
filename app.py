3c96a3441a124a2185dd2fc1271cd57374aeefea
AdeshSawa <asawaruth@outlook.com>
2024-04-07 23:12:49 +0400

    file name fix + gitignore

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ included_extensions = ['jpg','jpeg', 'bmp', 'png', 'gif']
 
 directory = f"{os.getcwd()}\{inputFolder}" #get directory path to input folder
 file_names = [fn for fn in os.listdir(directory)if any(fn.endswith(ext) for ext in included_extensions)] # get list of allowed image files from input folder
-print(file_names)
+# print(file_names)
 
 isExist = os.path.exists(outputFolder)
 if not isExist:
@@ -17,5 +17,11 @@ if not file_names:
     input("Press enter to exit;")
 else:
     for i in file_names:
-        cmd = 'cwebp "'+inputFolder+'/'+i+'" -o '+outputFolder+'/'+i+'.webp"'
-        os.system(cmd)
+        x = i.split(".")
+        if x[-1] in included_extensions:
+            x.remove(x[-1])
+            out_file_name = ''.join(x)
+            cmd = 'cwebp "'+inputFolder+'/'+i+'" -o '+outputFolder+'/'+out_file_name+'.webp"'
+            os.system(cmd)
+        else:
+            print("ERROR: "+i+" cannot be converted")
